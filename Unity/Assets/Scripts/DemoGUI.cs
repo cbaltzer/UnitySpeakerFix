b8a395cdb56796cc2a594276c6c00576053b2d8f
Christopher Baltzer <cbaltzer@gmail.com>
2013-07-13 12:13:13 -0300

    unity and demo gui

---
+++ Unity/Assets/Scripts/DemoGUI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class DemoGUI : MonoBehaviour {
+
+
+	private AudioClip newClip;
+	private bool _started;
+
+	void Start() {
+		_started = false;
+	}
+
+	void Break() {
+		if (Microphone.IsRecording(null)) {
+			End();
+		}
+
+		_started = true;
+		newClip = Microphone.Start(null, false, 100, 44100);
+	}
+
+	void End() {
+		_started = false;
+		Microphone.End(null);
+	}
+
+
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(100, 100, 100, 50), !_started ? "Break" : "End")) {
+
+			if (!_started) {
+				Break();
+			} else {
+				End();
+			}
+
+		}
+
+
+		if (GUI.Button(new Rect(100, 250, 100, 50), "Fix")) {
+			iPhoneSpeaker.Fix();
+		}
+
+	}
+
+}
---
+++ Unity/Assets/Scripts/iPhoneSpeaker.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class iPhoneSpeaker : MonoBehaviour {
+
+	
+
+	public static void Fix() {
+		Debug.Log("Fixing");
+
+	}
+
+}
